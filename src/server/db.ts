908769ef1ee9a36ed544431bde79164374e35a73
Pieter Venter <pieter@venter.pro>
2024-04-26 17:10:27 +0200

    add prisma acellerate extension

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../prisma/generated/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 import { env } from "~/env";
